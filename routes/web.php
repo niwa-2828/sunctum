8b70fb4f26802d05140769c8af7e6c8d9553ce30
niwa <khn.kazmasa@gmail.com>
2024-01-16 00:43:44 +0900

    sunctum

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//webでもAPIでもいい
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+    
+    //認証に対して権限を追加できる。
+    //return $user->createToken('token-name', ['server:update'])->plainTextToken;
+
+    //サーバーアップデートの権限が存在すれば。。。
+    // if ($user->tokenCan('server:update')) {
+    //     //
+    // }
+});
+
+//認証なしで呼び出されたら困るもののグループ化
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
